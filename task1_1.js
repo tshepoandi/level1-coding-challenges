76e0491be7b31b6b1ea28cc9ed458bf1df487e8f
tshepoandi <lovekilledtshepo@gmail.com>
2021-06-14 22:00:22 +0200

    added task 1.1 javascript file

---
+++ task1_1.js
@@ -0,0 +1,11 @@
+var multiplesOfThree = 0 
+var multiplesOfFive = 0 
+
+while (multiplesOfThree < 1000) {
+  multiplesOfThree = multiplesOfThree + 3
+}
+while (multiplesOfFive < 1000) {
+  multiplesOfFive = multiplesOfFive + 5
+} 
+
+console.log(multiplesOfFive + multiplesOfThree)
